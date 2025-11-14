a13e63bc54cfecd478ad04f1390b55e0b8326ddd
Marcel Susky <marcel.susky@tu-dresden.de>
2025-11-14 13:11:19 +0100

    Issue #28: Adds GH workflow to compare profile with ISiK, MIO and MHD profile

--- docs/compare.sh
+++ docs/compare.sh
@@ -1,28 +1,44 @@
 #/usr/bin/env sh
 
-java -jar ../validator_cli.jar \
-     -version 4.0.1 \
-     -compare \
-     -ig $(pwd)/../fsh-generated/resources \
-     -ig de.gematik.isik#5.1.0 \
-     -dest $(pwd)/mii-kds-dokument_isik \
-     -left https://www.medizininformatik-initiative.de/fhir/ext/modul-dokument/StructureDefinition/mii-pr-dokument-dokument \
-     -right https://gematik.de/fhir/isik/StructureDefinition/ISiKDokumentenMetadaten
+FHIR_VERSION="4.0.1"
+WORKSPACE=${1:-"$(pwd)"}
 
-java -jar ../validator_cli.jar \
-     -version 4.0.1 \
-     -compare \
-     -ig $(pwd)/../fsh-generated/resources \
-     -ig kbv.basis#1.7.0 \
-     -dest $(pwd)/mii-kds-dokument_mio \
-     -left https://www.medizininformatik-initiative.de/fhir/ext/modul-dokument/StructureDefinition/mii-pr-dokument-dokument \
-     -right https://fhir.kbv.de/StructureDefinition/KBV_PR_Base_DocumentReference
+ISIK_VERSION=${2:-"5.1.0"}
+ISIK_DOCUMENT_PROFILE="https://gematik.de/fhir/isik/StructureDefinition/ISiKDokumentenMetadaten"
 
-java -jar ../validator_cli.jar \
-     -version 4.0.1 \
+MIO_VERSION=${3:-"1.7.0"}
+MIO_DOCUMENT_PROFILE="https://fhir.kbv.de/StructureDefinition/KBV_PR_Base_DocumentReference"
+
+MHD_VERSION=${4:-"4.2.3"}
+MHD_DOCUMENT_PROFILE="https://profiles.ihe.net/ITI/MHD/StructureDefinition/IHE.MHD.UnContained.Comprehensive.DocumentReference"
+
+mkdir mii-kds-dokument_isik \
+      mii-kds-dokument_mio \
+      mii-kds-dokument_mhd
+
+java -jar ${WORKSPACE}/validator_cli.jar \
+     -version ${FHIR_VERSION} \
      -compare \
-     -ig $(pwd)/../fsh-generated/resources \
-     -ig ihe.iti.mhd#4.2.3 \
-     -dest $(pwd)/mii-kds-dokument_mhd \
+     -ig ${WORKSPACE}/fsh-generated/resources \
+     -ig de.gematik.isik#${ISIK_VERSION} \
+     -dest ${WORKSPACE}/docs/mii-kds-dokument_mhd \
      -left https://www.medizininformatik-initiative.de/fhir/ext/modul-dokument/StructureDefinition/mii-pr-dokument-dokument \
-     -right https://profiles.ihe.net/ITI/MHD/StructureDefinition/IHE.MHD.UnContained.Comprehensive.DocumentReference
+     -right ${ISIK_DOCUMENT_PROFILE}
+
+java -jar ${WORKSPACE}/validator_cli.jar \
+     -version ${FHIR_VERSION} \
+     -compare \
+     -ig ${WORKSPACE}/fsh-generated/resources \
+     -ig kbv.basis#${MIO_VERSION} \
+     -dest ${WORKSPACE}/docs/mii-kds-dokument_mhd \
+     -left https://www.medizininformatik-initiative.de/fhir/ext/modul-dokument/StructureDefinition/mii-pr-dokument-dokument \
+     -right ${MIO_DOCUMENT_PROFILE}
+
+java -jar ${WORKSPACE}/validator_cli.jar \
+     -version ${FHIR_VERSION} \
+     -compare \
+     -ig ${WORKSPACE}/fsh-generated/resources \
+     -ig ihe.iti.mhd#${MHD_VERSION} \
+     -dest ${WORKSPACE}/docs/mii-kds-dokument_mhd \
+     -left https://www.medizininformatik-initiative.de/fhir/ext/modul-dokument/StructureDefinition/mii-pr-dokument-dokument \
+     -right ${MHD_DOCUMENT_PROFILE}
